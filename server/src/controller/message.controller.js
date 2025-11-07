58daf39a3f5bee90ebc77ccd39da365c5885ee18
nazneen636 <nazneennahar636@gmail.com>
2025-11-07 21:34:57 +0600

    message

--- server/src/controller/message.controller.js
+++ server/src/controller/message.controller.js
@@ -3,7 +3,8 @@ import apiResponse from "../helpers/apiResponse.js";
 import customError from "../helpers/customError.js";
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
-export const getUserForSiderbar = asyncHandler(async (req, res) => {
+
+export const getUserForSidebar = asyncHandler(async (req, res) => {
   const userId = req.user._id;
   const filteredUsers = await User.find({ _id: { $ne: userId } }).select(
     "-password"
@@ -23,10 +24,41 @@ export const getUserForSiderbar = asyncHandler(async (req, res) => {
   if (!filteredUsers) {
     throw new customError(401, "message user not found");
   }
-  apiResponse.sendSuccess(res, 201, "unseen messages", {
+  apiResponse.sendSuccess(res, 201, "messages get successfully", {
     users: filteredUsers,
     unseenMessage,
   });
 });
 
 // get all messages for selected user
+export const getMessages = asyncHandler(async (req, res) => {
+  const { id: selectedUserId } = req.params;
+  const myId = req.user._id;
+  const messages = await Message.find({
+    $or: [
+      { senderId: myId, receiverId: selectedUserId },
+      {
+        senderId: selectedUserId,
+        receiverId: myId,
+      },
+    ],
+  });
+  await Message.updateMany(
+    { senderId: selectedUserId, receiverId: myId },
+    { seen: true }
+  );
+  if (!messages) {
+    throw new customError("something went wrong to get message");
+  }
+  apiResponse.sendSuccess(res, 201, "message seen", messages);
+});
+
+// api to mark message as seen using message id
+export const markMessageAsSeen = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const message = await Message.findByIdAndUpdate(id, { seen: true });
+  if (!message) {
+    throw new customError(401, "message not seen");
+  }
+  apiResponse.sendSuccess(res, 201, "message seen successfully");
+});
