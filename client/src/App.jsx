520dc1b6501710955a685aadb268f886b2c39b52
nazneen636 <nazneennahar636@gmail.com>
2025-11-09 10:58:57 +0600

    authcontext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,11 @@ import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 import LoginPage from "./pages/LoginPage";
+import { Toaster } from "react-hot-toast";
 const App = () => {
   return (
     <div className="bg-[url(/src/assets/bgImage.svg)] bg-no-repeat bg-center bg-cover">
+      <Toaster />
       <Routes>
         <Route index element={<HomePage />} />
         <Route path="/profile" element={<ProfilePage />} />
